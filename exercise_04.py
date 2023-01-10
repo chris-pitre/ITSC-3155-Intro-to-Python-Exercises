40b0f942a603cb1562ad3d64c91a4681d2c36895
eggsandwich-dev <cpitre1@uncc.edu>
2023-01-10 15:10:33 -0500

    Create exercise_04.py

---
+++ exercise_04.py
@@ -0,0 +1,10 @@
+numFloats = int(input("Enter a number: "))
+floatList = []
+
+for i in range(numFloats):
+    floatList.append(float(input("Enter number {}: ".format(i+1))))
+
+print("List: {}".format(floatList))
+average = sum(floatList)/len(floatList)
+
+print("Average: {}".format(average))
