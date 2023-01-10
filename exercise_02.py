a92357e0163240f962cd2e40c7dcf660b9969c56
eggsandwich-dev <cpitre1@uncc.edu>
2023-01-10 14:33:23 -0500

    exercise 2 and 3

---
+++ exercise_02.py
@@ -0,0 +1,4 @@
+firstString = input("Enter a string: ")
+secondString = input("Enter another string: ")
+
+print(firstString in secondString or secondString in firstString)
---
+++ exercise_03.py
@@ -0,0 +1,4 @@
+cubeInt = input("Enter an integer greater than 1: ")
+
+for i in range(cubeInt):
+    print("The cube of "+i+"is "+i**2)
