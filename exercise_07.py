1857babf4512db901e58afe6d11517d23804e567
eggsandwich-dev <cpitre1@uncc.edu>
2023-01-14 17:41:36 -0500

    exercise 7 comments

--- exercise_07.py
+++ exercise_07.py
@@ -15,6 +15,7 @@ while quitCase:
 
 print("All Nums: {}".format(numList))
 
+#List comprehension, if the modulo of a number in numList is equal to 0 (so if its an even number) it gets added to evenNums
 evenNums = [i for i in numList if i % 2 == 0]
 
 print("Even Nums: {}".format(evenNums))
